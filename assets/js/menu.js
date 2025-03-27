5adba109d7b0accd1a020ef02f14070bff3f14f7
Kevin Martinez <kevinmartinez08.97@hotmail.com>
2025-03-27 20:36:41 -0300

    Nuevos ajustes

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,8 +6,17 @@ $(document).ready(() => {
 
 
     setTimeout(() => {
-        document.querySelectorAll('a[rel="noreferrer"]').forEach(link => {
-            link.style.display = "none";
-        });
-    }, 2000);
+
+        $(".EkQXg").css({
+            'background': '#fff',
+            'box-shadow': '0 0 5px #0003',
+            'margin': '5px'
+        })
+
+        $(".dIBzjh").css({
+            'background': '#fff !important'
+        })
+        $('.elfsight-app-c258c807-c999-48fa-acb4-990dd3b6d0b5.eapps-widget.eapps-widget-show-toolbar > a').css({ 'display': 'none !important', 'opacity': '0 !important' })
+    }, 4000);
+
 })
